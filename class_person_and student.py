d9988ea342057e167faad5466a42365e325d7c03
janisfs <janisfs@mail.ru>
2024-06-24 16:20:03 +0300

    add class person and student.py

---
+++ class_person_and student.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"My name is {self.name} and I am {self.age} years old")
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def study(self):
+        print(f"{self.name} is now studying")
+
+Anton = Student("Anton", 18, 12345)
+
+Anton.introduce()
+Anton.study()
